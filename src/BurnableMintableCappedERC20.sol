ff2df9a5c04aa1c633ca60b57c505122c524d2f6
Kiryl Yermakou <rma4ok@gmail.com>
2022-02-16 15:18:54 -0500

    feature(BurnableERC20): burnFrom + permit

--- src/BurnableMintableCappedERC20.sol
+++ src/BurnableMintableCappedERC20.sol
@@ -7,8 +7,17 @@ import { DepositHandler } from './DepositHandler.sol';
 import { EternalStorage } from './EternalStorage.sol';
 
 contract BurnableMintableCappedERC20 is MintableCappedERC20 {
+    bytes32 public DOMAIN_SEPARATOR;
+
+    string private constant EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA = '\x19\x01';
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 private constant PERMIT_SIGNATURE_HASH =
+        bytes32(0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9);
+
     bytes32 private constant PREFIX_TOKEN_FROZEN = keccak256('token-frozen');
     bytes32 private constant KEY_ALL_TOKENS_FROZEN = keccak256('all-tokens-frozen');
+
+    mapping(address => uint256) public nonces;
 
     event Frozen(address indexed owner);
     event Unfrozen(address indexed owner);
@@ -18,7 +27,15 @@ contract BurnableMintableCappedERC20 is MintableCappedERC20 {
         string memory symbol,
         uint8 decimals,
         uint256 capacity
-    ) MintableCappedERC20(name, symbol, decimals, capacity) {}
+    ) MintableCappedERC20(name, symbol, decimals, capacity) {
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256('EIP712Domain(uint256 chainId,address verifyingContract)'),
+                block.chainid,
+                address(this)
+            )
+        );
+    }
 
     function depositAddress(bytes32 salt) public view returns (address) {
         /* Convert a hash which is bytes32 to an address which is 20-byte long
@@ -43,6 +60,36 @@ contract BurnableMintableCappedERC20 is MintableCappedERC20 {
     function burn(bytes32 salt) public onlyOwner {
         address account = depositAddress(salt);
         _burn(account, balanceOf[account]);
+    }
+
+    function burnFrom(address account, uint256 amount) public onlyOwner {
+        _approve(account, owner, allowance[account][owner] - amount);
+        _burn(account, amount);
+    }
+
+    function permit(
+        address issuer,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        require(block.timestamp < deadline, 'EXPIRED');
+
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                EIP191_PREFIX_FOR_EIP712_STRUCTURED_DATA,
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_SIGNATURE_HASH, issuer, spender, value, nonces[issuer]++, deadline))
+            )
+        );
+
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress == issuer, 'INVALID_SIGNATURE');
+
+        _approve(issuer, spender, value);
     }
 
     function _beforeTokenTransfer(
