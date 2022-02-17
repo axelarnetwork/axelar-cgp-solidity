d553cfb2fe5333eefcc98763cde81a0f7d35047d
Kiryl Yermakou <rma4ok@gmail.com>
2022-02-17 16:17:09 -0500

    chore: prettier

--- test/MintableCappedERC20.js
+++ test/MintableCappedERC20.js
@@ -2,9 +2,7 @@
 
 const chai = require('chai');
 const {
-  utils: {
-    splitSignature,
-  },
+  utils: { splitSignature },
 } = require('ethers');
 const { deployContract, MockProvider, solidity } = require('ethereum-waffle');
 chai.use(solidity);
@@ -18,10 +16,7 @@ const MintableCappedERC20 = require('../build/MintableCappedERC20.json');
 const { it } = require('mocha');
 
 describe('MintableCappedERC20', () => {
-  const [
-    ownerWallet,
-    userWallet,
-  ] = new MockProvider().getWallets();
+  const [ownerWallet, userWallet] = new MockProvider().getWallets();
   let token;
 
   beforeEach(async () => {
@@ -35,7 +30,7 @@ describe('MintableCappedERC20', () => {
       symbol,
       decimals,
       capacity,
-    ])
+    ]);
 
     const amount = 1000000;
     await token.mint(userWallet.address, amount);
@@ -43,24 +38,26 @@ describe('MintableCappedERC20', () => {
 
   describe('burning from account with given approve', () => {
     it('should burnFrom address', async () => {
-      const issuer = userWallet.address
-      const spender = ownerWallet.address
-      const amount = 1000
+      const issuer = userWallet.address;
+      const spender = ownerWallet.address;
+      const amount = 1000;
 
-      await expect( await token.connect(userWallet).approve(spender, amount))
-        .to.emit(token, 'Approval').withArgs(issuer, spender, amount)
+      await expect(await token.connect(userWallet).approve(spender, amount))
+        .to.emit(token, 'Approval')
+        .withArgs(issuer, spender, amount);
 
       await expect(await token.burnFrom(issuer, amount))
-        .to.emit(token, 'Transfer').withArgs(issuer, ADDRESS_ZERO, amount)
+        .to.emit(token, 'Transfer')
+        .withArgs(issuer, ADDRESS_ZERO, amount);
     });
-  })
+  });
   describe('EIP-2612: approve permit', () => {
     it('should should set allowance by verifying permit', async () => {
-      const issuer = userWallet.address
-      const spender = ownerWallet.address
-      const amount = 10000
-      const nonce = 0
-      const deadline = 1000 + (Date.now() / 1000) | 0
+      const issuer = userWallet.address;
+      const spender = ownerWallet.address;
+      const amount = 10000;
+      const nonce = 0;
+      const deadline = (1000 + Date.now() / 1000) | 0;
 
       const signature = splitSignature(
         await userWallet._signTypedData(
@@ -72,11 +69,11 @@ describe('MintableCappedERC20', () => {
           },
           {
             Permit: [
-              {name: 'owner', type: 'address'},
-              {name: 'spender', type: 'address'},
-              {name: 'value', type: 'uint256'},
-              {name: 'nonce', type: 'uint256'},
-              {name: 'deadline', type: 'uint256'},
+              { name: 'owner', type: 'address' },
+              { name: 'spender', type: 'address' },
+              { name: 'value', type: 'uint256' },
+              { name: 'nonce', type: 'uint256' },
+              { name: 'deadline', type: 'uint256' },
             ],
           },
           {
@@ -87,11 +84,21 @@ describe('MintableCappedERC20', () => {
             deadline,
           },
         ),
+      );
+
+      await expect(
+        await token.permit(
+          issuer,
+          spender,
+          amount,
+          deadline,
+          signature.v,
+          signature.r,
+          signature.s,
+        ),
       )
-
-      await expect( await token.permit(issuer, spender, amount, deadline, signature.v, signature.r, signature.s))
-        .to.emit(token, 'Approval').withArgs(issuer, spender, amount)
-
+        .to.emit(token, 'Approval')
+        .withArgs(issuer, spender, amount);
     });
   });
 });
