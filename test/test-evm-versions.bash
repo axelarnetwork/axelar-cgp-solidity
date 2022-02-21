2d414dfd42dc015d8cdf436ad1f0edefc7c225c4
Kiryl Yermakou <rma4ok@gmail.com>
2022-02-21 18:25:35 -0500

    fix(test-evm-versions): explicit error messages

--- test/test-evm-versions.bash
+++ test/test-evm-versions.bash
@@ -9,7 +9,14 @@ declare -a versions=(
 
 for version in "${versions[@]}"
 do
+  echo "Building and testing for EVM version: $version"
   rm -rf build
-  waffle compile "$configsPath/waffle-$version.json"
-  mocha
+  waffle compile "$configsPath/waffle-$version.json" || {
+    echo "Error: build failed for EVM version: $version"
+    exit 1
+  }
+  mocha || {
+   echo "Error: tests failed for EVM version: $version"
+   exit 1
+  }
 done
