e012f57573c3564c5b0429157b77aa6778f7bf87
Foivos <foivos@umich.edu>
2022-04-13 18:40:27 +0300

    removed call gateway functionality.

--- src/util/AxelarGasReceiver.sol
+++ src/util/AxelarGasReceiver.sol
@@ -10,7 +10,6 @@ import { ERC20Permit } from '../ERC20Permit.sol';
 
 // This should be owned by the microservice that is paying for gas.
 contract AxelarGasReceiver is Ownable{
-    IAxelarGateway public gateway;
     
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
@@ -60,12 +59,9 @@ contract AxelarGasReceiver is Ownable{
     }
 
     function setup(bytes calldata data) public {
-        address gateway_;
         ( 
-            owner, 
-            gateway_ 
-        ) = abi.decode(data, (address, address));
-        gateway = IAxelarGateway(gateway_);
+            owner
+        ) = abi.decode(data, (address));
     }
 
     //This is called by contracts that do stuff on the source chain before calling a remote contract.
@@ -94,63 +90,6 @@ contract AxelarGasReceiver is Ownable{
         emit GasReceivedWithToken(msg.sender, destinationChain, destinationAddress, payload, symbol, amountThrough, gasToken, gasAmount);
     }
 
-    //This is called by users to pay gas and send a remote contract call in one tx.
-    function receiveGasAndCallRemote (
-        string memory destinationChain,
-        string memory destinationAddress,
-        bytes calldata payload,
-        address gasToken,
-        uint256 gasAmount
-    ) external {
-        IERC20(gasToken).transferFrom(msg.sender, address(this), gasAmount);
-        emit GasReceived(msg.sender, destinationChain, destinationAddress, payload, gasToken, gasAmount);
-        gateway.callContract(destinationChain, destinationAddress, payload);
-    }
-
-    //This is called by users to pay gas and send a remote contract call in one tx.
-    function receiveGasAndCallRemoteWithToken (
-        string memory destinationChain,
-        string memory destinationAddress,
-        bytes calldata payload,
-        string memory symbol,
-        uint256 amountThrough,
-        address gasToken,
-        uint256 gasAmount
-    ) external {
-        IERC20(gasToken).transferFrom(msg.sender, address(this), gasAmount);
-        emit GasReceivedWithToken(msg.sender, destinationChain, destinationAddress, payload, symbol, amountThrough, gasToken, gasAmount);
-        IERC20 tokenThrough = IERC20(gateway.tokenAddresses(symbol));
-        tokenThrough.transferFrom(msg.sender, address(this), amountThrough);
-        tokenThrough.approve(address(gateway), amountThrough);
-        gateway.callContractWithToken(destinationChain, destinationAddress, payload, symbol, amountThrough);
-    }
-
-    //This is called by users to pay gas and send a remote contract call in one tx. It is cheaper in gas than the above.
-    function receiveGasAndCallRemoteWithTokenPermit (
-        string memory destinationChain,
-        string memory destinationAddress,
-        bytes calldata payload,
-        string memory symbol,
-        uint256 amountThrough,
-        address gasToken,
-        uint256 gasAmount,
-        uint256 deadline,
-        bytes memory signature
-    ) external {
-        IERC20(gasToken).transferFrom(msg.sender, address(this), gasAmount);
-        emit GasReceivedWithToken(msg.sender, destinationChain, destinationAddress, payload, symbol, amountThrough, gasToken, gasAmount);
-        {
-            (
-                uint8 v,
-                bytes32 r,
-                bytes32 s 
-            ) = abi.decode(signature, (uint8, bytes32, bytes32));
-            ERC20Permit tokenThrough = ERC20Permit(gateway.tokenAddresses(symbol));
-            tokenThrough.permit(msg.sender, address(gateway), amountThrough, deadline, v, r, s);
-        }
-        gateway.callContractWithToken(destinationChain, destinationAddress, payload, symbol, amountThrough);
-    }
-
     //This is called by contracts that do stuff on the source chain before calling a remote contract.
     function receiveGasNative (
         string memory destinationChain,
@@ -173,60 +112,6 @@ contract AxelarGasReceiver is Ownable{
         if(msg.value == 0)
             revert NothingReceived();
         emit GasReceivedNativeWithToken(msg.sender, destinationChain, destinationAddress, payload, symbol, amountThrough, msg.value);
-    }
-
-    //This is called by users to pay gas and send a remote contract call in one tx.
-    function receiveGasNativeAndCallRemote (
-        string memory destinationChain,
-        string memory destinationAddress,
-        bytes calldata payload
-    ) external payable {
-        if(msg.value == 0)
-            revert NothingReceived();
-        emit GasReceivedNative(msg.sender, destinationChain, destinationAddress, payload, msg.value);
-        gateway.callContract(destinationChain, destinationAddress, payload);
-    }
-
-    //This is called by users to pay gas and send a remote contract call in one tx.
-    function receiveGasNativeAndCallRemoteWithToken (
-        string memory destinationChain,
-        string memory destinationAddress,
-        bytes calldata payload,
-        string memory symbol,
-        uint256 amountThrough
-    ) external payable {
-        if(msg.value == 0)
-            revert NothingReceived();
-        emit GasReceivedNativeWithToken(msg.sender, destinationChain, destinationAddress, payload, symbol, amountThrough, msg.value);
-        IERC20 tokenThrough = IERC20(gateway.tokenAddresses(symbol));
-        tokenThrough.transferFrom(msg.sender, address(this), amountThrough);
-        tokenThrough.approve(address(gateway), amountThrough);
-        gateway.callContractWithToken(destinationChain, destinationAddress, payload, symbol, amountThrough);
-    }
-
-    //This is called by users to pay gas and send a remote contract call in one tx. It is cheaper in gas than the above.
-    function receiveGasNativeAndCallRemoteWithTokenPermit (
-        string memory destinationChain,
-        string memory destinationAddress,
-        bytes calldata payload,
-        string memory symbol,
-        uint256 amountThrough,
-        uint256 deadline,
-        bytes memory signature
-    ) external payable {
-        if(msg.value == 0)
-            revert NothingReceived();
-        emit GasReceivedNativeWithToken(msg.sender, destinationChain, destinationAddress, payload, symbol, amountThrough, msg.value);
-        {
-            (
-                uint8 v,
-                bytes32 r,
-                bytes32 s 
-            ) = abi.decode(signature, (uint8, bytes32, bytes32));
-            ERC20Permit tokenThrough = ERC20Permit(gateway.tokenAddresses(symbol));
-            tokenThrough.permit(msg.sender, address(gateway), amountThrough, deadline, v, r, s);
-        }
-        gateway.callContractWithToken(destinationChain, destinationAddress, payload, symbol, amountThrough);
     }
 
     function retreiveFees(address payable receiver, address[] memory tokens) external onlyOwner {
