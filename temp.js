2694d327d7174574e8842a2feb51f41dceff3dc3
Foivos <foivos@umich.edu>
2022-04-15 13:58:32 +0300

    removing a temp file.

--- temp.js
+++
@@ -1,8 +0,0 @@
-const { Contract, constants: {AddressZero}, getDefaultProvider } = require('ethers');
-const abi = require('./build/IAxelarGasReceiver.json');
-const provider = getDefaultProvider();
-const contract = new Contract(AddressZero, abi.abi, provider);
-(async () => {
-
-console.log(await contract.populateTransaction.setup('0x'));
-})();
