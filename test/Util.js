44fbc0695eb696cbfa624ba197baea03e8521105
Foivos <foivos@umich.edu>
2022-05-23 10:52:41 +0300

    made lint happy.

--- test/Util.js
+++ test/Util.js
@@ -1,21 +1,16 @@
 'use strict';
 
 const chai = require('chai');
-const {
-  utils: { splitSignature },
-} = require('ethers');
 const { deployContract, MockProvider, solidity } = require('ethereum-waffle');
 chai.use(solidity);
 const { expect } = chai;
-
-const CHAIN_ID = 1;
 
 const UtilTest = require('../build/UtilTest.json');
 
 const { it } = require('mocha');
 
 describe('UtilTest', () => {
-  const [ownerWallet, userWallet] = new MockProvider().getWallets();
+  const [ownerWallet] = new MockProvider().getWallets();
   let utilTest;
 
   beforeEach(async () => {
@@ -28,6 +23,7 @@ describe('UtilTest', () => {
       address.toLowerCase(),
     );
   });
+  
   it('should convert string of any format to address', async () => {
     const address = ownerWallet.address;
     expect(await utilTest.stringToAddress(address)).to.equal(address);
