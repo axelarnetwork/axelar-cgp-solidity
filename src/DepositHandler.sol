6cd6a03bcdf233fc9ebf0f05a1c3de423c8c6799
Sammy Liu <sammy.liu@axelar.network>
2022-06-09 02:49:08 +0800

    bring deposit handler back to v2.1.0 again

--- src/DepositHandler.sol
+++ src/DepositHandler.sol
@@ -3,32 +3,23 @@
 pragma solidity 0.8.9;
 
 contract DepositHandler {
-    error IsLocked();
-
-    uint256 internal constant IS_NOT_LOCKED = uint256(1);
-    uint256 internal constant IS_LOCKED = uint256(2);
+    uint256 internal constant IS_NOT_LOCKED = uint256(0);
+    uint256 internal constant IS_LOCKED = uint256(1);
 
     uint256 internal _lockedStatus = IS_NOT_LOCKED;
 
     modifier noReenter() {
-        if (_lockedStatus == IS_LOCKED) revert IsLocked();
+        require(_lockedStatus == IS_NOT_LOCKED);
 
         _lockedStatus = IS_LOCKED;
         _;
         _lockedStatus = IS_NOT_LOCKED;
     }
 
-    function execute(address callee, bytes calldata data)
-        external
-        noReenter
-        returns (bool success, bytes memory returnData)
-    {
-        if (callee.code.length != 0) {
-            (success, returnData) = callee.call(data);
-        }
+    function execute(address callee, bytes calldata data) external noReenter returns (bool success, bytes memory returnData) {
+        (success, returnData) = callee.call(data);
     }
 
-    // NOTE: The gateway should always destroy the `DepositHandler` in the same runtime context that deploys it.
     function destroy(address etherDestination) external noReenter {
         selfdestruct(payable(etherDestination));
     }
