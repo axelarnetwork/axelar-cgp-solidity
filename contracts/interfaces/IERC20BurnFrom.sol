5c2a2ae28ae491b2cea3ca8b673b2a7640160caf
Foivos <foivos@umich.edu>
2022-06-22 19:36:27 +0300

    reduce to ^0.8.0

--- contracts/interfaces/IERC20BurnFrom.sol
+++ contracts/interfaces/IERC20BurnFrom.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.9;
+pragma solidity ^0.8.0;
 
 interface IERC20BurnFrom {
     function burnFrom(address account, uint256 amount) external;
--- contracts/interfaces/IMintableCappedERC20.sol
+++ contracts/interfaces/IMintableCappedERC20.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.9;
+pragma solidity ^0.8.0;
 
 import { IERC20 } from './IERC20.sol';
 import { IERC20Permit } from './IERC20Permit.sol';
