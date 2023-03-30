13b0dcd1d559895bed0b7f99c64b6f100f884ade
Kiryl Yermakou <rma4ok@gmail.com>
2023-03-29 22:13:25 -0400

    fix(git): merge conflict

--- contracts/deposit-service/DepositServiceBase.sol
+++ contracts/deposit-service/DepositServiceBase.sol
@@ -6,7 +6,6 @@ import { StringToBytes32, Bytes32ToString } from '@axelar-network/axelar-gmp-sdk
 import { SafeTokenTransfer } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/utils/SafeTransfer.sol';
 import { IAxelarGateway } from '../interfaces/IAxelarGateway.sol';
 import { IDepositServiceBase } from '../interfaces/IDepositServiceBase.sol';
-import { Bytes32ToString, StringToBytes32 } from '../util/BytesStringUtil.sol';
 
 // This should be owned by the microservice that is paying for gas.
 abstract contract DepositServiceBase is IDepositServiceBase {
