40f52ef0eac249332d251817a96c0d7b0e2a10a5
Dean Amiel <damiel@deans-mbp.lan>
2023-06-30 04:12:54 -0400

    service governance tests and docs

--- contracts/interfaces/IAxelarServiceGovernance.sol
+++ contracts/interfaces/IAxelarServiceGovernance.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.9;
+pragma solidity ^0.8.0;
 
 import { IInterchainGovernance } from './IInterchainGovernance.sol';
 import { IMultisigBase } from './IMultisigBase.sol';
 
+/**
+ * @title IAxelarServiceGovernance Interface
+ * @dev This interface extends IInterchainGovernance and IMultisigBase for multisig proposal actions
+ */
 interface IAxelarServiceGovernance is IMultisigBase, IInterchainGovernance {
     error NotApproved();
 
@@ -12,5 +16,10 @@ interface IAxelarServiceGovernance is IMultisigBase, IInterchainGovernance {
     event MultisigCancelled(bytes32 indexed proposalHash);
     event MultisigExecuted(bytes32 indexed proposalHash);
 
+    /**
+     * @notice Executes a multisig proposal
+     * @param targetContract The target address the proposal will call
+     * @param callData The data that encodes the function and arguments to call on the target contract
+     */
     function executeMultisigProposal(address targetContract, bytes calldata callData) external payable;
 }
