c6e20db4f3e0454b4cc77d1bd514777719f2b766
Milap Sheth <milap@axelar.network>
2023-10-19 04:01:54 -0400

    fix flatten script

--- scripts/flatten-contracts.sh
+++ scripts/flatten-contracts.sh
@@ -17,7 +17,7 @@ rm -rf "$OUTPUT"
 mkdir -p "$OUTPUT"
 
 # Get compiler version, expected format ".*version: 'x.y.z',"
-version=$(grep 'version' ./hardhat.config.js | sed "s/version: '//" | sed "s/',$//g")
+version=$(grep 'version' ./hardhat.config.js | sed "s/ *version: '//" | sed "s/',$//g")
 
 # Flatten files
 find "$SOURCE" -name '*.sol' -print | while read -r file; do
@@ -28,20 +28,26 @@ find "$SOURCE" -name '*.sol' -print | while read -r file; do
     path="${file#${SOURCE}/}"
     mkdir -p "$OUTPUT"/"$(dirname "${path}")"
 
-    # Flatten contract, and remove hardhat comment at the top
-    hardhat flatten "$file" | tail -n +2 >"$OUTPUT/$path"
+    # Flatten contract
+    hardhat flatten "$file" > flat.sol
 
-    # Remove duplicate SPDX identifiers and pragmas that the explorers don't like
-    text=$(grep -vE "// SPDX.*" "$OUTPUT/$path" | grep -vE "pragma solidity .*")
+    # Remove hardhat comment at the top
+    tail -n+3 flat.sol > "$OUTPUT/$path"
+
+    # # Remove duplicate SPDX identifiers and pragmas that the explorers don't like
+    # text=$(grep -vE "// SPDX.*" "$OUTPUT/$path" | grep -vE "pragma solidity .*")
+    text=$(grep -vE "pragma solidity .*" "$OUTPUT/$path" | grep -vE "// Original license:.*")
 
     echo "// Source: $SOURCE/$path\n\n" > "$OUTPUT/$path"
-    echo "// SPDX-License-Identifier: MIT\n\n" >> "$OUTPUT/$path"
     echo "pragma solidity $version;\n\n" >> "$OUTPUT/$path"
     printf "%s" "$text" >> "$OUTPUT/$path"
 
     # Prettify source (in particular, remove extra newlines)
     prettier --write "$OUTPUT/$path"
 done
+
+# Delete temp file
+rm -f flat.sol
 
 if [ -z "$(ls -A $OUTPUT)" ]; then
     echo "No contracts from source $SOURCE/ were found at $OUTPUT"
