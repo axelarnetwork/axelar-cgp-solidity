0fb933430103c863e9804b790de5caa917f61fb1
Milap Sheth <milap@axelar.network>
2023-12-12 18:02:24 -0500

    feat: fix proxy bytecode compiler settings (#264)

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,31 +16,44 @@ const chains = require(`@axelar-network/axelar-chains-config/info/${env}.json`);
 const keys = readJSON(`${__dirname}/keys.json`);
 const { networks, etherscan } = importNetworks(chains, keys);
 
+const optimizerSettings = {
+    enabled: true,
+    runs: 1000,
+    details: {
+        peephole: process.env.COVERAGE === undefined,
+        inliner: process.env.COVERAGE === undefined,
+        jumpdestRemover: true,
+        orderLiterals: true,
+        deduplicate: true,
+        cse: process.env.COVERAGE === undefined,
+        constantOptimizer: true,
+        yul: true,
+        yulDetails: {
+            stackAllocation: true,
+        },
+    },
+};
+const compilerSettings = {
+    version: '0.8.9',
+    settings: {
+        evmVersion: process.env.EVM_VERSION || 'london',
+        optimizer: optimizerSettings,
+    },
+};
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
     solidity: {
-        version: '0.8.9',
-        settings: {
-            evmVersion: process.env.EVM_VERSION || 'london',
-            optimizer: {
-                enabled: true,
-                runs: 1000,
-                details: {
-                    peephole: process.env.COVERAGE === undefined,
-                    inliner: process.env.COVERAGE === undefined,
-                    jumpdestRemover: true,
-                    orderLiterals: true,
-                    deduplicate: true,
-                    cse: process.env.COVERAGE === undefined,
-                    constantOptimizer: true,
-                    yul: true,
-                    yulDetails: {
-                        stackAllocation: true,
-                    },
-                },
-            },
+        compilers: [compilerSettings],
+        // Fix the Proxy bytecodes
+        overrides: {
+            'contracts/AxelarGatewayProxy.sol': compilerSettings,
+            'contracts/BurnableMintableCappedERC20.sol': compilerSettings,
+            'contracts/DepositHandler.sol': compilerSettings,
+            'contracts/gas-service/AxelarGasServiceProxy.sol': compilerSettings,
+            'contracts/deposit-service/AxelarDepositServiceProxy.sol': compilerSettings,
         },
     },
     defaultNetwork: 'hardhat',
