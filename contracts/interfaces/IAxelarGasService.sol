232a3c41102a2c7b7b427413eefe22d40ab2a415
re1ro <rma4ok@gmail.com>
2024-03-25 19:42:41 -0400

    Update contracts/interfaces/IAxelarGasService.sol
    Co-authored-by: Milap Sheth <milap@axelar.network>

--- contracts/interfaces/IAxelarGasService.sol
+++ contracts/interfaces/IAxelarGasService.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
  * @title IAxelarGasService Interface
  * @notice This is an interface for the AxelarGasService contract which manages gas payments
  * and refunds for cross-chain communication on the Axelar network.
- * @dev This interface inherits IUpgradable
+ * @dev This interface is re-exported from axelar-gmp-sdk-solidity package
  */
 // solhint-disable-next-line no-unused-import
 import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
