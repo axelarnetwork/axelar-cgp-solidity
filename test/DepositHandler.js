17ffe07e4ac3a48e8eb5acd0214eb8daf5844d9d
Attiss Ngo <92927591+AttissNgo@users.noreply.github.com>
2025-04-03 21:24:31 -0500

    refactor DepositHandler tests

--- test/DepositHandler.js
+++ test/DepositHandler.js
@@ -45,34 +45,14 @@ describe('DepositHandler', function () {
     });
 
     describe('destroy', function () {
-        /* NOT TESTING AS DENCUN UPGRADE PREVENTS CALLING SELF DESTRUCT UNLESS IT HAPPENS IN THE SAME TX IN WHICH THE CONTRACT IS CREATED */
+        it('should destroy the contract and send ETH to destination', async function () {
+            await owner.sendTransaction({ to: test.address, value: 10 }).then((tx) => tx.wait());
+            await test.destroy(depositHandler.address).then((tx) => tx.wait());
 
-        // it('should destroy the contract and send ETH to destination', async function () {
-        //     await owner.sendTransaction({ to: test.address, value: 10 }).then((tx) => tx.wait());
-        //     await test.destroy(depositHandler.address).then((tx) => tx.wait());
+            await expect(depositHandler.destroy(user.address)).to.changeEtherBalance(user, 10);
 
-        //     ////
-        //     const userBalanceBefore = await ethers.provider.getBalance(user.address);
-        //     const codeBeforeDestroy = await ethers.provider.getCode(depositHandler.address);
-        //     console.log('Code before destroy:', codeBeforeDestroy.length > 0 ? 'Contract exists' : 'No contract');
-
-        //     // Destroy the contract and wait for the transaction to be confirmed
-        //     const destroyTx = await depositHandler.destroy(user.address);
-        //     await destroyTx.wait(2); // Wait for more confirmations
-
-        //     ////
-        //     const userBalanceAfter = await ethers.provider.getBalance(user.address);
-        //     console.log('Balance change:', userBalanceAfter.sub(userBalanceBefore).toString());
-        //     expect(userBalanceAfter.sub(userBalanceBefore)).to.equal(10);
-
-        //     // Wait a moment for the node to reflect the contract destruction
-        //     await new Promise((resolve) => setTimeout(resolve, 1000));
-
-        //     // Also verify that the contract was destroyed
-        //     const codeAfterDestroy = await ethers.provider.getCode(depositHandler.address);
-        //     console.log('Code after destroy:', codeAfterDestroy);
-        //     expect(codeAfterDestroy).to.equal('0x');
-        // });
+            expect(await ethers.provider.getCode(depositHandler.address)).to.equal('0x');
+        });
 
         it('should revert if locked (no reentrancy)', async function () {
             const data = depositHandler.interface.encodeFunctionData('destroy', [user.address]);
@@ -80,16 +60,13 @@ describe('DepositHandler', function () {
             await owner.sendTransaction({ to: test.address, value: 10 }).then((tx) => tx.wait());
             await test.destroy(depositHandler.address).then((tx) => tx.wait());
 
-            ////
             const userBalanceBefore = await ethers.provider.getBalance(user.address);
 
             await depositHandler.execute(depositHandler.address, data);
 
-            ////
             const userBalanceAfter = await ethers.provider.getBalance(user.address);
-            expect(userBalanceAfter).to.equal(userBalanceBefore); // No change in balance
+            expect(userBalanceAfter).to.equal(userBalanceBefore);
 
-            // Also verify the contract code remains
             expect(await ethers.provider.getCode(depositHandler.address)).to.not.equal('0x');
         });
     });
